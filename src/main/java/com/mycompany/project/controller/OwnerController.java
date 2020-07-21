ba843cc9b9a8e56f93a3c2714c69a64a874ed08d
dudwo123wjs <dudwo321wjs@naver.com>
2020-07-21 20:13:05 +0900

    sad
    sdsadsa

--- src/main/java/com/mycompany/project/controller/OwnerController.java
+++ src/main/java/com/mycompany/project/controller/OwnerController.java
@@ -3,7 +3,7 @@ package com.mycompany.project.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMapping;  
 
 import com.mycompany.project.model.OloginForm;
 
